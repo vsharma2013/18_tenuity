f7497b90f6e2b16ce492cd0dc8b710e7d69a7a24
Vishal Sharma <vishal2033@gmail.com>
2019-11-14 08:22:20 +0530

    item details from object complete

--- client/js/main.js
+++ client/js/main.js
@@ -1,4 +1,4 @@
-var item = {
+var item1 = {
   sku : 'S001',
   name : 'Gujiya',
   imgUrl : 'img/gujiya.png',
@@ -20,17 +20,26 @@ var item = {
   vendorLocation : 'Pune'
 };
 
+var strItem = JSON.stringify(item1);
+var item2 = JSON.parse(strItem);
+var item3 = JSON.parse(strItem);
+item2.sku = 'S002';
+item3.sku = 'S003';
+
+var items = [item1, item2, item3];
+
 $(document).ready(buildUI);
 
 
 function getItems() {
   var itemsPromise = new Promise(function(resolve, reject){
     setTimeout(function() {
-      resolve([item, item, item]);
+      resolve(items);
     }, 10);
   });
   return itemsPromise;
 }
+
 function buildUI() {
   getItems().then(function(items) {
     renderItems(items);
@@ -51,7 +60,7 @@ function renderItems(items) {
 function getCard(item){
   var cardHtml = `
   <div class="card">
-    <div class="details-icon">
+    <div class="details-icon"  sku=${item.sku}>
         <i class="fas fa-ellipsis-v"></i>
     </div>     
   <div class="card-content" rating=${item.rating}>
@@ -83,7 +92,6 @@ function reRenderRatings(){
       var i = 0;
       for(var iKey in Object.keys(allRatingIcons)){
         if(i < rating){
-          console.log($(allRatingIcons[iKey]));
           $(allRatingIcons[iKey]).addClass('f-c-gold');
         }
         i++;
@@ -92,10 +100,79 @@ function reRenderRatings(){
   });
 }
 
+function renderItemDetails(itemSku) {
+  var itemDetailsContainer = $('#item-details');
+  itemDetailsContainer.empty();
+  var item = items.find(i => {
+    return i.sku == itemSku;
+  })
+  var detailsHtml=`
+    <div id="item-details-container">
+      <div class="p-1"><h1 class="clr-red">Item Details</h1></div>
+      @PLACE_HOLDER_KEY_VALUES
+    </div>
+  `;
+  
+  var keyValues = ''
+  var keys = [{ id: 'ingredients', display: 'Ingredients'},
+              { id: 'nutritionInfo', display: 'Nutrition'},
+              { id: 'taste', display: 'Taste'},
+              { id: 'cookingStyle', display: 'Cooking Style'},
+              { id: 'shelfLife', display: 'Shelf Life'},
+              { id: 'dispatchTime', display: 'Dispatch Time'}, 
+              { id: 'healthy', display: 'Healthy'},
+              { id: 'vendorName', display: 'Vendor Name'},
+              { id: 'fssai', display: 'FSSAI'},
+              { id: 'vendorRating', display: 'Vendor Rating'},
+              { id: 'vendorLocation', display: 'Vendor Location'}
+            ];
+  
+  keys.forEach(key => {
+    var value = item[key.id];
+    if(typeof(value) === 'object'){
+      var str = `
+      <div class="details-key-value-container">
+        <div class="detail detail-key">Ingredients</div>
+        <div class="detail detail-value">
+        @PLACE_HOLDER_KEY_VALUE
+        </div>
+      </div>
+      `;
+      var kv = '';
+      for(vk in value) {
+        var v = value[vk];
+        var s = v.isPresent ?
+                `<span class="clr-red"> ${v.name}</span> |` :
+                `<span> ${v.name}</span> |`;
+        kv += s;
+      }
+      str = str.replace('@PLACE_HOLDER_KEY_VALUE', kv)
+      keyValues += str;
+    } else{
+      var str = `
+      <div class="details-key-value-container">
+          <div class="detail detail-key">${key.display}</div>
+          <div class="detail detail-value"><span class="clr-red">${value}</span></div>
+      </div>
+      `;
+      keyValues += str;
+    }
+  });  
+  str = `
+  <div class="details-key-value-container">
+      <a href="" class="btn btn-back">Back</a>
+  </div> 
+  `;
+  keyValues += str;       
+  detailsHtml = detailsHtml.replace('@PLACE_HOLDER_KEY_VALUES', keyValues);
+  $(detailsHtml).appendTo(itemDetailsContainer);
+  itemDetailsContainer.show();
+}
+
 function addEventHandlers() {
-  $('.details-icon').on('click', function() {
+  $('.details-icon').on('click', function(e) {
+    renderItemDetails($(e.currentTarget).attr('sku'));
     $('#cards-container').hide();
-    $('#item-details').show();
   });
 
   $('.btn-back').on('click', function() {
